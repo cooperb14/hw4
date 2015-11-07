3bc16e0b0dbfaf82116e767b8d5491e8b64b5433
Cooper Bates <cooperb14@gmail.com>
2015-11-06 19:50:30 -0500

    Syncs files with local repo

--- effects_tester.py
+++ effects_tester.py
@@ -6,16 +6,20 @@ import effects
 def main():
     print('\nPortable Pixmap (PPM) Image Editor!\n')
     print('Choose the effect you would like to try:')
-    print('1) object_filter\n2) shades_of_gray\n3) negate_red')
-    print('4) negate_green\n5) negate_blue\n6) mirror\n')
-    effect_num = input('Enter a number: \n')
-    in_filename = input('Enter an input file name: \n')
-    out_filename = input('Enter an output file name: \n')
-    print(out_filename, ' created.') 
     
-    x = [0] * 6
-    x[effect_num] = 1
-    
-    effects.apply_effects(in_filename, out_filename, x)
+    answer = 'y'
+    while answer.lower() == 'y':
+        print('1) object_filter\n2) shades_of_gray\n3) negate_red')
+        print('4) negate_green\n5) negate_blue\n6) mirror\n')
+        effect_num = int(input('Enter a number: \n'))
+        in_filename = input('Enter an input file name: \n')
+        out_filename = input('Enter an output file name: \n')
+        print(out_filename, ' created.') 
+        
+        x = [0] * 6
+        x[effect_num - 1] = 1
+        answer = input('Do you wish to choose another effect? y/n')
+        #effects.apply_effects(in_filename, out_filename, x)
+    print(x)
 
 main()
